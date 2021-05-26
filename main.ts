da970891e0d0d9b2ec8c3d9071e76f79b2ec4089
nono38 <74985866+nono38@users.noreply.github.com>
2021-05-26 11:47:16 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,29 @@ radio.setGroup(207)
 basic.forever(function () {
     p_1 = pins.analogReadPin(AnalogPin.P1)
     // mal traduit
-    p_1 = Math.round(Math.map(p_1, 4, 1007, -100, 100))
+    p_1 = Math.round(Math.map(p_1, 4, 1007, -1, 1))
     basic.pause(200)
     p_2 = pins.analogReadPin(AnalogPin.P2)
     // mal traduit
-    p_2 = Math.round(Math.map(p_1, 4, 1007, -100, 100))
-    basic.pause(200)
+    p_2 = Math.round(Math.map(p_2, 4, 1007, -1, 1))
     if (p_1 == 0 && p_2 == 0) {
-        radio.sendValue("p_1 p_2", 0)
-    } else {
-        radio.sendValue("p_1", p_1)
+        radio.sendNumber(0)
+    } else if (p_1 == 0 && p_2 == 1) {
+        radio.sendNumber(1)
+    } else if (p_1 == 1 && p_2 == 0) {
+        radio.sendNumber(10)
+    } else if (p_1 == 0 && p_2 == -1) {
+        radio.sendNumber(-1)
+    } else if (p_1 == -1 && p_2 == 0) {
+        radio.sendNumber(-10)
+    } else if (p_1 == -1 && p_2 == 1) {
+        radio.sendNumber(-1.1)
+    } else if (p_1 == -1 && p_2 == -1) {
+        radio.sendNumber(-11)
+    } else if (p_1 == 1 && p_2 == -1) {
+        radio.sendNumber(12)
+    } else if (p_1 == 1 && p_2 == 1) {
+        radio.sendNumber(11)
     }
-    if (p_2 == 0) {
-    	
-    } else {
-        radio.sendValue("p_2", p_2)
-    }
+    basic.pause(200)
 })
